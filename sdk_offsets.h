0923ecd1e209657f400c4c09acb0c09b99b09d7a
Saturn <franco.schirra2000@gmail.com>
2026-04-09 14:55:58 +0200

    update offsets

--- sdk_offsets.h
+++ sdk_offsets.h
@@ -2,9 +2,9 @@
 #include <cstdint>
 
 // --- Global Offsets ---
-static constexpr uintptr_t OFFSET_GOBJECTS       = 0xE8A10F0;
-static constexpr uintptr_t OFFSET_GNAMES         = 0xE7E92C0;
-static constexpr uintptr_t OFFSET_GWORLD         = 0xEA4AB10;
+static constexpr uintptr_t OFFSET_GOBJECTS       = 0xE8A4170;
+static constexpr uintptr_t OFFSET_GNAMES         = 0xE7EC340;
+static constexpr uintptr_t OFFSET_GWORLD         = 0xEA4DBA0;
 
 // --- UObject Internals (layout never changes) ---
 static constexpr uintptr_t OFF_UObject_Class     = 0x0010;
