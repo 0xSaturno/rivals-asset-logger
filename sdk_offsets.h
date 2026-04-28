47049918734d03767132967ba3cd48d525529483
Saturn <franco.schirra2000@gmail.com>
2026-04-28 14:01:37 +0200

    update offsets

--- sdk_offsets.h
+++ sdk_offsets.h
@@ -6,7 +6,7 @@ static constexpr uintptr_t OFFSET_GOBJECTS       = 0xEBAD9B0;
 static constexpr uintptr_t OFFSET_GNAMES         = 0xEAF5B80;
 static constexpr uintptr_t OFFSET_GWORLD         = 0xED574E0;
 
-// --- UObject Internals (layout never changes) ---
+// --- UObject Internals ---
 static constexpr uintptr_t OFF_UObject_Class     = 0x0010;
 static constexpr uintptr_t OFF_UObject_FName     = 0x0018;
 static constexpr uintptr_t OFF_UObject_Outer     = 0x0028;
